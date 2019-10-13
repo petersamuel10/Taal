58265494272961ebedc96eb5903451a66387efe9
Mario Gamal <mario@vavisa-kw.com>
2019-10-13 18:32:28 +0300

    prepare displaying description fields

--- app/src/main/java/com/vavisa/taal/ui/main/requests/BindingAdapters.java
+++ app/src/main/java/com/vavisa/taal/ui/main/requests/BindingAdapters.java
@@ -1,6 +1,8 @@
 package com.vavisa.taal.ui.main.requests;
 
 
+import android.widget.TextView;
+
 import androidx.databinding.BindingAdapter;
 import androidx.recyclerview.widget.RecyclerView;
 
@@ -8,6 +10,7 @@ import com.google.android.material.tabs.TabLayout;
 import com.squareup.picasso.Picasso;
 import com.vavisa.taal.R;
 import com.vavisa.taal.data.model.Case;
+import com.vavisa.taal.data.model.DescriptionField;
 
 import java.util.List;
 
@@ -30,7 +33,21 @@ public class BindingAdapters {
     }
 
     @BindingAdapter("imageUrl")
-    public static void setImageUrl(CircleImageView imageView, String imageUrl){
+    public static void setImageUrl(CircleImageView imageView, String imageUrl) {
         Picasso.get().load(imageUrl).placeholder(R.drawable.image_placholder).into(imageView);
+    }
+
+    @BindingAdapter("descriptionFields")
+    public static void setDescriptionFields(TextView textView, List<DescriptionField> fields) {
+        if (fields != null)
+            textView.setText(getDescription(fields));
+    }
+
+    private static String getDescription(List<DescriptionField> fields) {
+        StringBuilder description = new StringBuilder();
+        for (DescriptionField field : fields) {
+            description.append(field.getLabel().concat(" : ").concat(field.getValue()).concat("\n"));
+        }
+        return description.toString();
     }
 }
